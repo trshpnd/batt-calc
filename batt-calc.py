617c5746980d963d79ea4cddb1fecfc9decb8dab
trshpnd <73907087+trshpnd@users.noreply.github.com>
2022-09-01 13:22:25 -0300

    Update batt-calc.py

--- batt-calc.py
+++ batt-calc.py
@@ -6,11 +6,11 @@ import numpy as np
 root = tk.Tk()
 root.withdraw()
 
+# função de integração
 def integrate(x, y):
     return np.trapz(y=y, x=x)
 
 # Índice das colunas de interesse no arquivo .csv
-
 col_time_index = 0          # Coluna timestamp
 col_current_index = 1       # Coluna corrente
 
@@ -18,13 +18,12 @@ file_dir = filedialog.askopenfilename()
 
 df = pd.read_csv(file_dir)
 
-# Aquisição do título das colunas para uso com iloc
-
+# Aquisição do título das colunas para uso com loc
 col_time_label = df.columns[col_time_index]
 col_current_label = df.columns[col_current_index]
 
 df = df.loc[(df[col_current_label] >= 0) & (df[col_time_label] >= 67.0)]        # remoção de valores inconsistentes
-df = df.iloc[:,[col_time_index,col_current_index]]                              # remoção de colunas irrelevantes
+df = df.loc[:,[col_time_label,col_current_label]]                               # remoção de colunas irrelevantes
 
 time = df[col_time_label].values
 current = df[col_current_label].values
